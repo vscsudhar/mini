3c48730e3be9c625ba6d5958f097ae6fbe6fa222
sudharsanrankuhigher <sudharsanrankuheigher@gmail.com>
2026-03-28 10:20:28 +0530

    fix: add start script

--- backend/config/connectDatabase.js
+++ backend/config/connectDatabase.js
@@ -1,9 +1,15 @@
 const mongoose = require('mongoose');
 
 const connectDatabase = () => {
-    mongoose.connect(process.env.DB_URL).then((data) => {
-        console.log(`Mongodb connected with server: ${data.connection.host}`)
+    mongoose.connect(process.env.DB_URL,
+        
+    )
+    .then((data) => {
+        console.log(`MongoDB connected with server: ${data.connection.host}`);
     })
-}
+    .catch((err) => {
+        console.log("MongoDB connection error:", err);
+    });
+};
 
 module.exports = connectDatabase;
